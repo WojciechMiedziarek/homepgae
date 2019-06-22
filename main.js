41597b502caccbe5f27ed05a35feea9b4b76419e
wojtek <wojciech.miedziarek@gmail.com>
2019-06-22 15:38:47 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('siemanko, witam i uszanowanko :)');
+const name='Wojtek';
+const age= 29;
+const hobby= 'malowanie figurek do gier bitewnych'
+console.log(name);
+console.log(age);
+console.log(hobby);
+
+
+console.log(`Nazywam się ${name} i mam ${age} lat. Moje hobby to ${hobby}`);
