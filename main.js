b1a9e26ab5cbf3274677cfa397effd13b707fc27
wojtek <wojciech.miedziarek@gmail.com>
2019-07-05 16:18:51 +0200

    BEM ale chyba bez sensu

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 console.log('siemanko, witam i uszanowanko :)');
-const name='Wojtek';
-const age= 29;
-const hobby= 'malowanie figurek do gier bitewnych'
+const name = 'Wojtek';
+const age = 29;
+const hobby = 'malowanie figurek do gier bitewnych'
 console.log(name);
 console.log(age);
 console.log(hobby);
 
 
 console.log(`Nazywam się ${name} i mam ${age} lat. Moje hobby to ${hobby}`);
+const header__title = document.querySelector('.header__title--js');
+console.log(header__title);
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+console.log(paragraphs[3]);
